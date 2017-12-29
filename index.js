5655520d234a68374c8fc9b65de3515afa3599e8
erha19 <faterrole@gmail.com>
2017-12-29 12:43:56 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const express = require('express');
+const http = require('http');
+const WebSocketServer = require('ws').Server;
+const ip = require('ip').address();
+const chalk = require('chalk');
+const app = express();
+const open = require("open");
+
+app.use(express.static('./'));
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
+let clients = [];
+
+wss.on('connection', function(ws) {
+    clients.push(ws);
+    ws.on('message', function(message) {
+        clients.forEach(function(ws1){
+            if(ws1 !== ws) {
+                ws1.send(message);
+            }
+       })
+    });
+
+    ws.on('close', function(message) {
+        clients = clients.filter(function(ws1){
+            return ws1 !== ws
+        })
+    });
+});
+
+server.listen(8080, function listening() {
+  const url = 'http://'+ ip + ':' + server.address().port;
+  const wsurl = 'ws://'+ ip + ':' + server.address().port;
+  console.log('Static server:')
+  console.log(chalk.green(url));
+  console.log('You can test websocket:')
+  console.log(chalk.green(wsurl));
+  open(url)
+});
